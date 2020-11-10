3d567a0531f42204e81ff5db11130686d7c0e247
igholm <ian.g.holm@intel.com>
2020-11-09 16:11:34 -0800

    Initial checkin

---
+++ ExceptionSerialization/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ExceptionSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+
+            try
+            {
+                throw new ArgumentException();
+            }
+            catch (ArgumentException ex)
+            {
+                var exJson = JsonConvert.SerializeObject(ex);
+                var exception = JsonConvert.DeserializeObject(exJson);
+
+                Console.WriteLine(exception);
+                Console.ReadLine();
+            }
+        }
+    }
+}
